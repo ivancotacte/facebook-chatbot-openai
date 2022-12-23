9df8e3d96097de54f7e094b3731c12f46643210e
miruchi <kaptenmojo@gmail.com>
2022-12-23 13:27:16 +0700

    Demo code

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const login = require("fb-chat-api");
+
+async function Openai() {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: event.body,
+        temperature: 0,
+        max_tokens: 7,
+      });
+      return response;
+}
+login({appState: JSON.parse(fs.readFileSync('./session.json', 'utf8'))}, (err, api) => {
+    if(err) return console.error(err);
+
+    api.setOptions({listenEvents: true});
+
+    api.listenMqtt((err, event) => {
+        if(err) return console.error(err);
+
+        api.markAsRead(event.threadID, (err) => {
+            if(err) console.error(err);
+        });
+  	if (event.type == "message") {
+        require('./handler.js')(api,event)
+    }
+    });
+});
